2c27b0fc7ec0622389956581fa17535e50158ca2
rachel490 <ccori12@naver.com>
2026-01-16 18:21:55 +0900

    feat(#1): add zod schemas for runtime type validation

---
+++ src/lib/api/validation.ts
@@ -0,0 +1,21 @@
+import z, { ZodType } from 'zod';
+
+export const fetchWithValidation = async <T>(
+  shema: ZodType<T>,
+  fetcher: () => Promise<unknown>,
+): Promise<T> => {
+  const data = await fetcher();
+  const result = shema.safeParse(data);
+
+  if (!result.success) {
+    throw new Error(
+      JSON.stringify({
+        status: 422,
+        message: 'Invlaid response from server',
+        errors: z.flattenError(result.error).fieldErrors,
+      }),
+    );
+  }
+
+  return result.data;
+};
