6532160f9f71c16063dbbcdefe65e57b16a459f7
rachel490 <ccori12@naver.com>
2026-01-19 19:30:20 +0900

    style(#9): add Pretendard font & configure Tailwind CSS variable

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,8 @@ export default async function Home() {
   const data = await clientApi.get<Todo[]>('/todos');
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
+    <div className="flex min-h-screen items-center justify-center bg-background">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
         {data?.map(item => (
           <div key={item.id}>{item.title}</div>
         ))}
