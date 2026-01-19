cc787b11b342bef81e1bcbad57def69b0355ef54
rachel490 <ccori12@naver.com>
2026-01-19 21:10:02 +0900

    style(#9): add typography design tokens

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,13 @@ export default async function Home() {
     <div className="flex min-h-screen items-center justify-center bg-background">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
         {data?.map(item => (
-          <div key={item.id}>{item.title}</div>
+          <div key={item.id}>
+            <div className="heading-s">{item.title}</div>
+            <div className="heading-r">{item.title}</div>
+            <div className="heading-m">{item.title}</div>
+            <div className="heading-b">{item.title}</div>
+            <div className="label-s">{item.title}</div>
+          </div>
         ))}
       </main>
     </div>
