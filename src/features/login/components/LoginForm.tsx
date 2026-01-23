e0e5d0efc18298b8a95732521d545ef40c79f46f
rachel490 <ccori12@naver.com>
2026-01-23 14:37:33 +0900

    feat(#11): add form validation in login page
    - Extract common schema field for reuse
    - Apply shared validation btw signup & login forms

---
+++ src/features/login/components/LoginForm.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import z from 'zod';
+import Link from 'next/link';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import Button from '@/src/shared/components/button/Button';
+import TextField from '@/src/shared/components/text-field/TextField';
+import { emailSchema, passwordSchema } from '../../signup/components/SignUpForm';
+
+const loginFormSchema = z.object({
+  email: emailSchema,
+  password: passwordSchema,
+});
+
+type LoginFormValues = z.infer<typeof loginFormSchema>;
+
+const LoginForm = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<LoginFormValues>({
+    resolver: zodResolver(loginFormSchema),
+    mode: 'all',
+  });
+
+  const onSubmit = (data: LoginFormValues) => {
+    alert(JSON.stringify(data));
+  };
+
+  return (
+    <form className="flex w-[328px] flex-col gap-6" onSubmit={handleSubmit(onSubmit)}>
+      <div className="flex flex-col gap-3">
+        <TextField
+          label="아이디"
+          placeholder="이메일 주소를 입력해 주세요."
+          {...register('email')}
+          messageType={errors.email && 'error'}
+          message={errors.email?.message}
+        />
+        <TextField
+          label="비밀번호"
+          placeholder="비밀번호를 입력해 주세요."
+          {...register('password')}
+          messageType={errors.password && 'error'}
+          message={errors.password?.message}
+        />
+      </div>
+      <div className="flex w-full flex-col gap-6">
+        <Button type="submit" variant="primary">
+          로그인
+        </Button>
+        <Link href={'/signup'} className="label-m text-text-primary text-center">
+          회원가입
+        </Link>
+      </div>
+    </form>
+  );
+};
+
+export default LoginForm;
