c8f3767eb50f5e6fabed14e8c08c4c6a4e453232
rachel490 <ccori12@naver.com>
2026-01-28 01:18:57 +0900

    feat(Timer): prompt login modal on timer start for nonauthenticated users

--- app/(navbar)/home/page.tsx
+++ app/(navbar)/home/page.tsx
@@ -1,11 +1,42 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import TimerStart from '@/src/shared/assets/svg/timer-start.svg';
 import TimerPause from '@/src/shared/assets/svg/timer-pause.svg';
+import { checkIsLoggedIn } from '@/src/features/auth/auth.action';
+import { useModal } from '@/src/lib/store/modalSlice';
 
 const TimerStop = () => {
   return <div className="h-25 w-25 rounded-[8px] bg-current" />;
 };
 
-export default async function Home() {
+type TimerMode = 'start' | 'stop' | 'pause';
+
+export default function Home() {
+  const router = useRouter();
+  const [mode, setMode] = useState<TimerMode>('start');
+  const onOpen = useModal(state => state.onOpen);
+
+  const handleStartClick = async () => {
+    const isLoggedIn = await checkIsLoggedIn();
+
+    console.log('isLoggedIn', isLoggedIn);
+
+    if (!isLoggedIn) {
+      const isConfirmClicked = await onOpen({
+        title: '로그인이 필요합니다.',
+        description: 'DevTime을 사용하려면 로그인이 필요합니다. 로그인 페이지로 이동할까요?',
+        buttons: [
+          { label: '취소', variant: 'secondary', action: 'cancel' },
+          { label: '로그인하기', variant: 'primary', action: 'confirm' },
+        ],
+      });
+
+      return isConfirmClicked ? router.push('/login') : null;
+    }
+  };
+
   return (
     <div className="mx-auto max-w-[1032px]">
       <div className="my-[96px] flex flex-col items-center gap-[50px]">
@@ -44,19 +75,20 @@ export default async function Home() {
 
           <div className="flex justify-center gap-20">
             <button
-              disabled
+              disabled={mode !== 'start'}
+              onClick={handleStartClick}
               className="disabled:text-background-primary-light text-content-primary"
             >
               <TimerStart />
             </button>
             <button
-              disabled
+              disabled={mode !== 'pause'}
               className="disabled:text-background-primary-light text-content-primary"
             >
               <TimerPause />
             </button>
             <button
-              disabled
+              disabled={mode !== 'stop'}
               className="disabled:text-background-primary-light text-content-primary"
             >
               <TimerStop />
