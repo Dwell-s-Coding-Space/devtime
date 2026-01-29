9c4a19a1027c240dd79d08b3cfb85f2e1735f1b8
rachel490 <ccori12@naver.com>
2026-01-29 23:50:04 +0900

    feat(styles): extend tailwind-merge for custom typography classes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,40 @@
 import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { extendTailwindMerge } from 'tailwind-merge';
+
+const twMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      'font-size': [
+        'heading-b',
+        'heading-s',
+        'heading-m',
+        'heading-r',
+        'title-b',
+        'title-s',
+        'title-m',
+        'title-r',
+        'subtitle-b',
+        'subtitle-s',
+        'subtitle-m',
+        'subtitle-r',
+        'body-b',
+        'body-s',
+        'body-m',
+        'body-r',
+        'label-b',
+        'label-s',
+        'label-m',
+        'label-r',
+        'caption-b',
+        'caption-s',
+        'caption-m',
+        'caption-r',
+        'timer',
+        'heading-dashboard',
+      ],
+    },
+  },
+});
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
