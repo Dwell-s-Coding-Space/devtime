93ff9098f6ead987175b7fae633ec288b0a941a3
rachel490 <ccori12@naver.com>
2026-01-31 22:12:34 +0900

    feat(TaskModal): add task completion, edit and delete functionality

--- src/features/timer/components/TaskModal/TaskItem.tsx
+++ src/features/timer/components/TaskModal/TaskItem.tsx
@@ -1,23 +1,84 @@
+import { ChangeEvent, MouseEvent, useState } from 'react';
+import { TaskMode } from '@/app/(navbar)/home/page';
+import { cn } from '@/src/lib/utils';
 import SymbolIcon from '@/src/shared/assets/svg/symbol.svg';
 import EditIcon from '@/src/shared/assets/svg/edit.svg';
 import TrashIcon from '@/src/shared/assets/svg/trash.svg';
+import CheckIcon from '@/src/shared/assets/svg/check.svg';
+import Checkbox from '@/src/shared/components/checkbox/Checkbox';
+import type { TaskItem as TaskItemType } from '@/src/features/dashboard/dashboard.schema';
 
 interface TaskItemProps {
-  task: string;
+  task: TaskItemType;
+  mode: TaskMode;
+  handleChange?: (data: Partial<TaskItemType>) => void;
+  handleDelete?: (id: string) => void;
 }
 
-const TaskItem = ({ task }: TaskItemProps) => {
+const TaskItem = ({ task, mode, handleChange, handleDelete }: TaskItemProps) => {
+  const checkboxId = `task-checkbox-${task.id}`;
+
+  const [isEdit, setIsEdit] = useState(false);
+  const [value, setValue] = useState(task.content || '');
+
+  const handleCheck = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!handleChange) return;
+
+    handleChange({ ...task, isCompleted: e.target.checked });
+  };
+
+  const handleEdit = (e: MouseEvent<HTMLButtonElement>) => {
+    setIsEdit(true);
+  };
+
+  const handleRealEdit = () => {
+    if (!handleChange) return;
+    handleChange({ ...task, content: value });
+    setIsEdit(false);
+  };
+
+  const onDelete = () => {
+    if (!handleDelete) return;
+    handleDelete(task.id);
+  };
+
   return (
-    <div className="bg-background-primary flex h-18 items-center gap-4 rounded-[8px] p-6">
+    <label
+      htmlFor={checkboxId}
+      className={cn('bg-background-primary flex h-18 items-center gap-4 rounded-[8px] p-6', {
+        'bg-background-disabled-dark': task.isCompleted,
+      })}
+    >
       <SymbolIcon className="text-background-white/50 h-5 w-[42px]" />
-      <span className="body-s text-text-white flex-1">{task}</span>
-      <button>
-        <EditIcon className="h-6 w-6 text-white" />
-      </button>
-      <button>
-        <TrashIcon className="h-6 w-6 text-white" />
-      </button>
-    </div>
+
+      <input
+        value={value}
+        onChange={e => setValue(e.target.value)}
+        readOnly={isEdit ? false : true}
+        className="body-s text-text-white flex-1"
+      />
+
+      {mode === 'edit' &&
+        (isEdit ? (
+          <button onClick={handleRealEdit}>
+            <CheckIcon className="h-6 w-6 text-white" />
+          </button>
+        ) : (
+          <>
+            <button onClick={handleEdit}>
+              <EditIcon className="h-6 w-6 text-white" />
+            </button>
+
+            <button onClick={onDelete}>
+              <TrashIcon className="h-6 w-6 text-white" />
+            </button>
+          </>
+        ))}
+
+      {mode === 'check' && (
+        <Checkbox id={checkboxId} checked={task.isCompleted} onChange={handleCheck} />
+      )}
+    </label>
   );
 };
 
