087985bb3aae089fa2a9735467c239997d79f160
rachel490 <ccori12@naver.com>
2026-01-31 22:12:34 +0900

    feat(TaskModal): add task completion, edit and delete functionality

--- src/features/timer/components/TaskModal/TaskList.tsx
+++ src/features/timer/components/TaskModal/TaskList.tsx
@@ -1,29 +1,52 @@
 import { ComponentProps } from 'react';
+import type { TaskMode } from '@/app/(navbar)/home/page';
 import { cn } from '@/src/lib/utils';
 import EditIcon from '@/src/shared/assets/svg/edit.svg';
+import type { TaskItem as TaskItemType } from '@/src/features/dashboard/dashboard.schema';
 import TaskItem from './TaskItem';
-
-type TaskMode = 'check' | 'view' | 'edit';
 
 interface TaskListProps extends ComponentProps<'div'> {
   mode: TaskMode;
-  tasks: string[];
+  tasks: TaskItemType[];
+  handleChange?: (data: Partial<TaskItemType>) => void;
+  handleDelete?: (id: string) => void;
+  changeTaskMode?: (newMode: TaskMode) => void;
 }
 
-const TaskList = ({ mode, tasks, className }: TaskListProps) => {
+const TaskList = ({
+  mode,
+  tasks,
+  className,
+  handleChange,
+  handleDelete,
+  changeTaskMode,
+}: TaskListProps) => {
+  const changeToEditMode = () => {
+    if (!changeTaskMode) return;
+    changeTaskMode('edit');
+  };
+
   return (
     <div className={cn('flex min-h-0 flex-1 flex-col gap-6', className)}>
       <div className="flex items-center gap-4">
         <h3 className="text-text-g700 title-b flex-1">할 일 목록</h3>
-        <button className="flex items-center gap-2">
-          <EditIcon className="text-text-g600 h-6 w-6" />
-          {mode === 'check' && <span className="label-m text-text-g600">할 일 수정</span>}
-        </button>
+        {mode === 'check' && (
+          <button className="flex items-center gap-2" onClick={changeToEditMode}>
+            <EditIcon className="text-text-g600 h-6 w-6" />
+            <span className="label-m text-text-g600">할 일 수정</span>
+          </button>
+        )}
       </div>
 
       <div className="flex min-h-0 flex-1 flex-col gap-3 overflow-y-auto">
         {tasks.map(task => (
-          <TaskItem key={task} task={task} />
+          <TaskItem
+            key={task.id}
+            task={task}
+            mode={mode}
+            handleChange={handleChange}
+            handleDelete={handleDelete}
+          />
         ))}
       </div>
     </div>
