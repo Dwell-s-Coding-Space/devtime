0e0d826cc9ccfead294373386c118e6cc3e1a20d
rachel490 <ccori12@naver.com>
2026-01-31 23:59:21 +0900

    feat(TaskModal): add shared layout and footer components

---
+++ src/features/timer/components/TaskModal/TaskModalLayout.tsx
@@ -0,0 +1,28 @@
+import { ReactNode, useEffect } from 'react';
+import Portal from '@/src/shared/components/modal/Portal';
+
+const TaskModalLayout = ({ children }: { children: ReactNode }) => {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, []);
+
+  return (
+    <Portal>
+      <div
+        className="bg-dim-b50 fixed top-0 left-0 z-50 flex h-screen w-full items-center justify-center overflow-hidden"
+        style={{
+          paddingBlock: `clamp(30px, calc(126 / 1080 * 100vh), 126px)`,
+        }}
+      >
+        <div className="bg-background-white mx-auto flex h-full w-full max-w-[640px] flex-col gap-9 rounded-[12px] p-9 pt-12">
+          {children}
+        </div>
+      </div>
+    </Portal>
+  );
+};
+
+export default TaskModalLayout;
