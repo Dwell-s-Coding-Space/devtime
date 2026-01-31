87b02aa43f0ad55042effaf51237fddab44ed8fc
rachel490 <ccori12@naver.com>
2026-01-31 23:59:36 +0900

    refactor(Timer): simplify modals using shared hooks and components

--- src/features/timer/components/TimerStartModal.tsx
+++ src/features/timer/components/TimerStartModal.tsx
@@ -1,26 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useMutation } from '@tanstack/react-query';
-import Button from '@/src/shared/components/button/Button';
-import Portal from '@/src/shared/components/modal/Portal';
 import { clientApi } from '@/src/lib/api/client';
-import type { TaskMode } from '@/app/(navbar)/home/page';
-import { type TaskItem } from '../../dashboard/dashboard.schema';
 import { createTimerApi } from '../timer.api';
-import Goal from './TaskModal/Goal';
-import AddTaskItem from './TaskModal/AddTaskItem';
-import TaskList from './TaskModal/TaskList';
-
-export interface TaskModalProps {
-  onClose: () => void;
-  onConfirm?: () => void;
-  onCancel?: () => void;
-  changeTaskMode?: (newMode: TaskMode) => void;
-}
+import type { TaskModalProps } from '../timer.types';
+import { useTasks } from '../hooks';
+import { TaskModalFooter, TaskModalLayout, Goal, AddTaskItem, TaskList } from './TaskModal';
 
 const TimerStartModal = ({ onClose }: TaskModalProps) => {
   const [goal, setGoal] = useState('');
-  const [newTodo, setNewTodo] = useState('');
-  const [todos, setTodos] = useState<TaskItem[]>([]);
+  const { newTaskContent, setNewTaskContent, tasks, addTask, deleteTask, updateTask } = useTasks(
+    {}
+  );
 
   const { mutate } = useMutation({
     mutationFn: createTimerApi(clientApi).postStart,
@@ -33,76 +23,22 @@ const TimerStartModal = ({ onClose }: TaskModalProps) => {
     },
   });
 
-  useEffect(() => {
-    document.body.style.overflow = 'hidden';
-    return () => {
-      document.body.style.overflow = '';
-    };
-  }, []);
-
-  const handleAddTodo = () => {
-    if (!newTodo) return;
-    if (todos.some(todo => todo.content === newTodo)) {
-      alert('이미 같은 할 일이 존재합니다.');
-      return;
-    }
-
-    setTodos(prev => [...prev, { id: `id-${newTodo}`, content: newTodo, isCompleted: false }]);
-    setNewTodo('');
-  };
-
-  const onTimerStart = () => {
-    mutate({ todayGoal: goal, tasks: todos.map(todo => todo.content) });
-  };
-
-  const handleTaskItemUpdate = (data: Partial<TaskItem>) => {
-    const updatedTodos = todos.map(todo => (todo.id === data.id ? { ...todo, ...data } : todo));
-    setTodos(updatedTodos);
-  };
-
-  const handleTaskItemDelete = (id: string) => {
-    const updatedTodos = todos.filter(todo => todo.id !== id);
-    setTodos(updatedTodos);
+  const handleStart = () => {
+    mutate({ todayGoal: goal, tasks: tasks.map(task => task.content) });
   };
 
   return (
-    <Portal>
-      <div
-        className="bg-dim-b50 fixed top-0 left-0 z-50 flex h-screen w-full items-center justify-center overflow-hidden"
-        style={{
-          paddingBlock: `clamp(30px, calc(126 / 1080 * 100vh), 126px)`,
-        }}
-      >
-        <div className="bg-background-white mx-auto flex h-full w-full max-w-[640px] flex-col gap-9 rounded-[12px] p-9 pt-12">
-          <Goal mode="edit" value={goal} onChange={e => setGoal(e.target.value)} />
-          <AddTaskItem
-            value={newTodo}
-            onChange={e => setNewTodo(e.target.value)}
-            onKeyDown={e => {
-              if (e.key !== 'Enter') return;
-              handleAddTodo();
-            }}
-            onClick={handleAddTodo}
-          />
-
-          <TaskList
-            tasks={todos}
-            mode="edit"
-            handleChange={handleTaskItemUpdate}
-            handleDelete={handleTaskItemDelete}
-          />
-
-          <div className="flex items-center justify-end gap-4">
-            <Button variant="tertiary" onClick={onClose}>
-              취소
-            </Button>
-            <Button variant="secondary" onClick={onTimerStart}>
-              타이머 시작하기
-            </Button>
-          </div>
-        </div>
-      </div>
-    </Portal>
+    <TaskModalLayout>
+      <Goal mode="edit" value={goal} onChange={e => setGoal(e.target.value)} />
+      <AddTaskItem
+        value={newTaskContent}
+        onChange={e => setNewTaskContent(e.target.value)}
+        onKeyDown={e => e.key === 'Enter' && addTask()}
+        onClick={addTask}
+      />
+      <TaskList tasks={tasks} mode="edit" handleChange={updateTask} handleDelete={deleteTask} />
+      <TaskModalFooter onCancel={onClose} onConfirm={handleStart} confirmText="타이머 시작하기" />
+    </TaskModalLayout>
   );
 };
 
--- src/features/timer/components/TimerStopModal.tsx
+++ src/features/timer/components/TimerStopModal.tsx
@@ -1,30 +1,35 @@
-import { ChangeEvent, useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import Button from '@/src/shared/components/button/Button';
-import Portal from '@/src/shared/components/modal/Portal';
 import { clientApi } from '@/src/lib/api/client';
 import type { TaskMode } from '@/app/(navbar)/home/page';
-import { GetStudyLogDetailResponse, type TaskItem } from '../../dashboard/dashboard.schema';
-import { createTimerApi } from '../timer.api';
-import AddTaskItem from './TaskModal/AddTaskItem';
-import TaskList from './TaskModal/TaskList';
 import Label from '@/src/shared/components/text-field/Label';
+import { GetStudyLogDetailResponse } from '../../dashboard/dashboard.schema';
+import { createTimerApi } from '../timer.api';
+import { TaskModalProps } from '../timer.types';
+import { useTasks } from '../hooks';
+import { TaskModalFooter, TaskModalLayout, AddTaskItem, TaskList } from './TaskModal';
 
-export interface TaskModalProps {
-  onClose: () => void;
+export interface TimerStopModalProps extends TaskModalProps {
   data?: GetStudyLogDetailResponse['data'];
-  changeTaskMode?: (newMode: TaskMode) => void;
   timerId?: string;
 }
 
-const TimerStopModal = ({ onClose, data, timerId }: TaskModalProps) => {
-  const { tasks } = data || {};
+const TimerStopModal = ({ onClose, data, timerId }: TimerStopModalProps) => {
   const queryClient = useQueryClient();
-
-  const [newTodo, setNewTodo] = useState('');
-  const [todos, setTodos] = useState<TaskItem[]>(tasks || []);
   const [taskMode, setTaskMode] = useState<TaskMode>('check');
   const [reflection, setReflection] = useState('');
+
+  const {
+    tasks,
+    newTaskContent,
+    setNewTaskContent,
+    addTask,
+    updateTask,
+    deleteTask,
+    getTasksPayload,
+  } = useTasks({
+    initialTasks: data?.tasks,
+  });
 
   const { mutate } = useMutation({
     mutationFn: createTimerApi(clientApi).putTasks,
@@ -50,124 +55,64 @@ const TimerStopModal = ({ onClose, data, timerId }: TaskModalProps) => {
     },
   });
 
-  useEffect(() => {
-    document.body.style.overflow = 'hidden';
-    return () => {
-      document.body.style.overflow = '';
-    };
-  }, []);
-
-  const handleAddTodo = () => {
-    if (!newTodo) return;
-    if (todos.some(todo => todo.content === newTodo)) {
-      alert('이미 같은 할 일이 존재합니다.');
-      return;
-    }
-
-    setTodos(prev => [...prev, { id: `id-${newTodo}`, content: newTodo, isCompleted: false }]);
-    setNewTodo('');
-  };
-
   const changeTaskMode = (newTaskMode: TaskMode) => {
     setTaskMode(newTaskMode);
   };
 
-  const handleTaskItemUpdate = (data: Partial<TaskItem>) => {
-    const updatedTodos = todos.map(todo => (todo.id === data.id ? { ...todo, ...data } : todo));
-    setTodos(updatedTodos);
-  };
-
-  const handleTaskItemDelete = (id: string) => {
-    const updatedTodos = todos.filter(todo => todo.id !== id);
-    setTodos(updatedTodos);
-  };
-
   const handleSave = () => {
-    if (!todos?.length || !data) return;
-    const _todos = todos.map(todo => ({ content: todo.content, isCompleted: todo.isCompleted }));
-    mutate({ studyLogId: data.id, reqBody: { tasks: _todos } });
-  };
-
-  const handleReflection = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    const value = e.target.value;
-
-    if (!value) return;
-    setReflection(value);
+    if (!tasks?.length || !data) return;
+    mutate({ studyLogId: data.id, reqBody: { tasks: getTasksPayload() } });
   };
 
   const handleFinish = () => {
-    if (!todos?.length || !data || !timerId) return;
+    if (!tasks?.length || !data || !timerId) return;
 
-    const _todos = todos.map(todo => ({
-      content: todo.content,
-      isCompleted: todo.isCompleted,
-    }));
-
-    const body = { review: reflection, tasks: _todos, splitTimes: [] };
-
-    mutateStop({ id: timerId, reqBody: body });
+    mutateStop({
+      id: timerId,
+      reqBody: { review: reflection, tasks: getTasksPayload(), splitTimes: [] },
+    });
   };
 
   return (
-    <Portal>
-      <div
-        className="bg-dim-b50 fixed top-0 left-0 z-50 flex h-screen w-full items-center justify-center overflow-hidden"
-        style={{
-          paddingBlock: `30px`,
-        }}
-      >
-        <div className="bg-background-white mx-auto flex h-full w-full max-w-[640px] flex-col gap-9 rounded-[12px] p-9 pt-12">
-          <div className="flex flex-col gap-1">
-            <h2 className="title-b">오늘도 수고하셨어요!</h2>
-            <span className="body-m text-text-g500">
-              완료한 일을 체크하고, 오늘의 학습 회고를 작성해 주세요.
-            </span>
-          </div>
-          <AddTaskItem
-            value={newTodo}
-            onChange={e => setNewTodo(e.target.value)}
-            onKeyDown={e => {
-              if (e.key !== 'Enter') return;
-              handleAddTodo();
-            }}
-            onClick={handleAddTodo}
-          />
-
-          <TaskList
-            tasks={todos}
-            mode={taskMode}
-            handleChange={handleTaskItemUpdate}
-            handleDelete={handleTaskItemDelete}
-            changeTaskMode={changeTaskMode}
-          />
-
-          <div className="flex flex-col gap-2">
-            <Label htmlFor="reflection">학습 회고</Label>
-            <textarea
-              value={reflection}
-              onChange={handleReflection}
-              className="bg-background-gray-light placeholder:text-text-disabled-300 body-m text-text-g600 h-[84px] overflow-y-scroll rounded-[5px] px-4 py-3"
-              placeholder="오늘 학습한 내용을 회고해 보세요(15자 이상 작성 필수)."
-            />
-          </div>
-
-          <div className="flex items-center justify-end gap-4">
-            <Button variant="tertiary" onClick={onClose}>
-              취소
-            </Button>
-            {taskMode === 'edit' ? (
-              <Button variant="secondary" onClick={handleSave}>
-                변경 사항 저장하기
-              </Button>
-            ) : (
-              <Button variant="secondary" onClick={handleFinish}>
-                공부 완료하기
-              </Button>
-            )}
-          </div>
-        </div>
+    <TaskModalLayout>
+      <div className="flex flex-col gap-1">
+        <h2 className="title-b">오늘도 수고하셨어요!</h2>
+        <span className="body-m text-text-g500">
+          완료한 일을 체크하고, 오늘의 학습 회고를 작성해 주세요.
+        </span>
       </div>
-    </Portal>
+
+      <AddTaskItem
+        value={newTaskContent}
+        onChange={e => setNewTaskContent(e.target.value)}
+        onKeyDown={e => e.key !== 'Enter' && addTask()}
+        onClick={addTask}
+      />
+
+      <TaskList
+        tasks={tasks}
+        mode={taskMode}
+        handleChange={updateTask}
+        handleDelete={deleteTask}
+        changeTaskMode={changeTaskMode}
+      />
+
+      <div className="flex flex-col gap-2">
+        <Label htmlFor="reflection">학습 회고</Label>
+        <textarea
+          value={reflection}
+          onChange={e => setReflection(e.target.value)}
+          className="bg-background-gray-light placeholder:text-text-disabled-300 body-m text-text-g600 h-[84px] overflow-y-scroll rounded-[5px] px-4 py-3"
+          placeholder="오늘 학습한 내용을 회고해 보세요(15자 이상 작성 필수)."
+        />
+      </div>
+
+      <TaskModalFooter
+        onCancel={onClose}
+        onConfirm={taskMode === 'edit' ? handleSave : handleFinish}
+        confirmText={taskMode === 'edit' ? '변경 사항 저장하기' : '공부 완료하기'}
+      />
+    </TaskModalLayout>
   );
 };
 
