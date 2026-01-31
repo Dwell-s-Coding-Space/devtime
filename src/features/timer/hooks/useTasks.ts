a3d34191b807d7cc5f03485ef127cc356af42cd1
rachel490 <ccori12@naver.com>
2026-01-31 23:58:55 +0900

    feat(Timer): add timer types and useTasks hook

---
+++ src/features/timer/hooks/useTasks.ts
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { TaskItem } from '../../dashboard/dashboard.schema';
+
+const useTasks = ({ initialTasks = [] }: { initialTasks?: TaskItem[] }) => {
+  const [newTaskContent, setNewTaskContent] = useState('');
+  const [tasks, setTasks] = useState<TaskItem[]>(initialTasks);
+
+  const addTask = () => {
+    if (!newTaskContent.trim()) return;
+    if (tasks.some(task => task.content === newTaskContent)) return;
+
+    setTasks(prev => [
+      ...prev,
+      { id: `id-${newTaskContent}`, content: newTaskContent, isCompleted: false },
+    ]);
+    setNewTaskContent('');
+  };
+
+  const deleteTask = (taskId: string) => {
+    const updatedTasks = tasks.filter(task => task.id !== taskId);
+    setTasks(updatedTasks);
+  };
+
+  const updateTask = (data: Partial<TaskItem>) => {
+    const updatedTasks = tasks.map(task => (task.id === data.id ? { ...task, ...data } : task));
+    setTasks(updatedTasks);
+  };
+
+  const getTasksPayload = () => {
+    return tasks.map(({ content, isCompleted }) => ({ content, isCompleted }));
+  };
+
+  return {
+    addTask,
+    deleteTask,
+    updateTask,
+    setNewTaskContent,
+    tasks,
+    newTaskContent,
+    getTasksPayload,
+  };
+};
+
+export default useTasks;
