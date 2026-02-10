b0186ee0a82a9262ade75042b3006045a6a9926a
rachel490 <ccori12@naver.com>
2026-02-10 20:35:36 +0900

    chore: debugging code for proxy

--- src/lib/api/server.ts
+++ src/lib/api/server.ts
@@ -4,6 +4,9 @@ import { cookies } from 'next/headers';
 export const createServerApi = async () => {
   const cookieStore = await cookies();
 
+  console.log('[createServerApi] backend url', process.env.API_BASE_URL);
+  console.log('[createServerApi] site url', process.env.SITE_URL);
+
   return new Api(`${process.env.SITE_URL}/api/proxy`, {
     headers: {
       'Content-Type': 'application/json',
