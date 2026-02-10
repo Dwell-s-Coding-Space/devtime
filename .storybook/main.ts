f53ca5aa1320ae368572587d992c0d789a1cd94e
rachel490 <ccori12@naver.com>
2026-02-10 23:58:17 +0900

    fix: configure Storybook SVGR support for SVG imports

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,29 @@
 import type { StorybookConfig } from '@storybook/nextjs-vite';
+import svgr from 'vite-plugin-svgr';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@chromatic-com/storybook',
+    '@storybook/addon-vitest',
+    '@storybook/addon-a11y',
+    '@storybook/addon-docs',
   ],
-  "addons": [
-    "@chromatic-com/storybook",
-    "@storybook/addon-vitest",
-    "@storybook/addon-a11y",
-    "@storybook/addon-docs"
-  ],
-  "framework": "@storybook/nextjs-vite",
-  "staticDirs": [
-    "../public"
-  ]
+  framework: {
+    name: '@storybook/nextjs-vite',
+    options: {
+      image: {
+        excludeFiles: ['**/*.svg'],
+      },
+    },
+  },
+  staticDirs: ['../public'],
+  viteFinal: async config => {
+    config.plugins = [
+      ...(config.plugins ?? []),
+      svgr({ include: /\.svg$/, svgrOptions: { exportType: 'default' } }),
+    ];
+    return config;
+  },
 };
 export default config;
