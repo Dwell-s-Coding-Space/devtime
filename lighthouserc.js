f5e12d7b45a08e511dd99a73a88abe95f829408f
rachel490 <ccori12@naver.com>
2026-02-11 15:30:20 +0900

    feat(ci): add Lighthouse CI auth for protected routes

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,11 +4,19 @@ module.exports = {
       startServerCommand: 'pnpm start',
       startServerReadyPattern: 'Ready in',
       url: [
+        'http://localhost:3000/home',
         'http://localhost:3000/login',
         'http://localhost:3000/signup',
-        'http://localhost:3000/home',
+        'http://localhost:3000/dashboard',
+        'http://localhost:3000/ranking',
+        'http://localhost:3000/mypage',
+        'http://localhost:3000/mypage/edit',
       ],
       numberOfRuns: 3,
+      puppeteerScript: './scripts/lighthouse-auth.js',
+      puppeteerLaunchOptions: {
+        args: ['--no-sandbox'],
+      },
     },
     assert: {
       assertions: {
