1f86038eaae8d9f7b36363642f5267d077f67db2
rachel490 <ccori12@naver.com>
2026-02-16 23:45:45 +0900

    feat: apply error boundary with QueryErrorResetBoundary to all pages

--- app/(navbar)/mypage/edit/page.tsx
+++ app/(navbar)/mypage/edit/page.tsx
@@ -1,11 +1,27 @@
+'use client';
+
+import { QueryErrorResetBoundary } from '@tanstack/react-query';
 import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 
 import { MyPageEdit, MyPageEditLoading } from '@/src/features/mypage';
+import ErrorBoundaryFallback from '@/src/shared/components/error-boundary/ErrorBoundaryFallback';
 
 export default function MypageEdit() {
   return (
-    <Suspense fallback={<MyPageEditLoading />}>
-      <MyPageEdit />
-    </Suspense>
+    <QueryErrorResetBoundary>
+      {({ reset }) => (
+        <ErrorBoundary
+          onReset={reset}
+          fallbackRender={({ resetErrorBoundary }) => (
+            <ErrorBoundaryFallback onRetry={resetErrorBoundary} className="h-[600px]" />
+          )}
+        >
+          <Suspense fallback={<MyPageEditLoading />}>
+            <MyPageEdit />
+          </Suspense>
+        </ErrorBoundary>
+      )}
+    </QueryErrorResetBoundary>
   );
 }
--- app/(navbar)/mypage/page.tsx
+++ app/(navbar)/mypage/page.tsx
@@ -1,11 +1,27 @@
+'use client';
+
+import { QueryErrorResetBoundary } from '@tanstack/react-query';
 import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 
 import { MyPage, MyPageLoading } from '@/src/features/mypage';
+import ErrorBoundaryFallback from '@/src/shared/components/error-boundary/ErrorBoundaryFallback';
 
 export default function Mypage() {
   return (
-    <Suspense fallback={<MyPageLoading />}>
-      <MyPage />
-    </Suspense>
+    <QueryErrorResetBoundary>
+      {({ reset }) => (
+        <ErrorBoundary
+          onReset={reset}
+          fallbackRender={({ resetErrorBoundary }) => (
+            <ErrorBoundaryFallback onRetry={resetErrorBoundary} className="h-[420px]" />
+          )}
+        >
+          <Suspense fallback={<MyPageLoading />}>
+            <MyPage />
+          </Suspense>
+        </ErrorBoundary>
+      )}
+    </QueryErrorResetBoundary>
   );
 }
