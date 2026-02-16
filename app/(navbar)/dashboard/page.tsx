646e30126265196695e2b9350667bc2f620a0dec
rachel490 <ccori12@naver.com>
2026-02-16 23:45:45 +0900

    feat: apply error boundary with QueryErrorResetBoundary to all pages

--- app/(navbar)/dashboard/page.tsx
+++ app/(navbar)/dashboard/page.tsx
@@ -1,27 +1,35 @@
 'use client';
-import { QueryErrorResetBoundary, useQuery, useQueryClient } from '@tanstack/react-query';
+
+import { QueryErrorResetBoundary } from '@tanstack/react-query';
 import { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 
 import { StatSummary, StudyLogList, WeeklyStatBar, YearlyStatGrid } from '@/src/features/dashboard';
-import { dashboardQueries } from '@/src/features/dashboard/dashboard.queries';
-// import ErrorBoundary from '@/src/shared/components/error-boundary/ErrorBoundary';
 import ErrorBoundaryFallback from '@/src/shared/components/error-boundary/ErrorBoundaryFallback';
 import Skeleton from '@/src/shared/components/skeleton/Skeleton';
 
 export default function Dashboard() {
-  const queryClient = useQueryClient();
-  const { refetch } = useQuery(dashboardQueries.heatmap());
-
   return (
     <>
       <div className="flex flex-col gap-4">
-        <Suspense fallback={<Skeleton className="h-[264px] bg-gray-300" />}>
-          <div className="flex items-center gap-4">
-            <StatSummary />
-            <WeeklyStatBar />
-          </div>
-        </Suspense>
+        <QueryErrorResetBoundary>
+          {({ reset }) => (
+            <ErrorBoundary
+              onReset={reset}
+              fallbackRender={({ resetErrorBoundary }) => (
+                <ErrorBoundaryFallback onRetry={resetErrorBoundary} className="h-[264px]" />
+              )}
+            >
+              <Suspense fallback={<Skeleton className="h-[264px] bg-gray-300" />}>
+                <div className="flex items-center gap-4">
+                  <StatSummary />
+                  <WeeklyStatBar />
+                </div>
+              </Suspense>
+            </ErrorBoundary>
+          )}
+        </QueryErrorResetBoundary>
+
         <QueryErrorResetBoundary>
           {({ reset }) => (
             <ErrorBoundary
@@ -30,15 +38,27 @@ export default function Dashboard() {
                 <ErrorBoundaryFallback onRetry={resetErrorBoundary} className="h-[300px]" />
               )}
             >
-              <Suspense fallback={<Skeleton className="h-[300px]" />}>
+              <Suspense fallback={<Skeleton className="h-[300px] bg-gray-300" />}>
                 <YearlyStatGrid />
               </Suspense>
             </ErrorBoundary>
           )}
         </QueryErrorResetBoundary>
-        <Suspense fallback={<Skeleton className="h-[500px] bg-gray-300" />}>
-          <StudyLogList />
-        </Suspense>
+
+        <QueryErrorResetBoundary>
+          {({ reset }) => (
+            <ErrorBoundary
+              onReset={reset}
+              fallbackRender={({ resetErrorBoundary }) => (
+                <ErrorBoundaryFallback onRetry={resetErrorBoundary} className="h-[500px]" />
+              )}
+            >
+              <Suspense fallback={<Skeleton className="h-[500px] bg-gray-300" />}>
+                <StudyLogList />
+              </Suspense>
+            </ErrorBoundary>
+          )}
+        </QueryErrorResetBoundary>
       </div>
     </>
   );
--- app/(navbar)/ranking/page.tsx
+++ app/(navbar)/ranking/page.tsx
@@ -1,11 +1,27 @@
+'use client';
+
+import { QueryErrorResetBoundary } from '@tanstack/react-query';
 import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 
 import { RankingPage, RankingPageLoading } from '@/src/features/ranking';
+import ErrorBoundaryFallback from '@/src/shared/components/error-boundary/ErrorBoundaryFallback';
 
 export default function Ranking() {
   return (
-    <Suspense fallback={<RankingPageLoading />}>
-      <RankingPage />
-    </Suspense>
+    <QueryErrorResetBoundary>
+      {({ reset }) => (
+        <ErrorBoundary
+          onReset={reset}
+          fallbackRender={({ resetErrorBoundary }) => (
+            <ErrorBoundaryFallback onRetry={resetErrorBoundary} className="h-[500px]" />
+          )}
+        >
+          <Suspense fallback={<RankingPageLoading />}>
+            <RankingPage />
+          </Suspense>
+        </ErrorBoundary>
+      )}
+    </QueryErrorResetBoundary>
   );
 }
