8748bedfaa5e0b2015b336c3f9ad040511b896cc
rachel490 <ccori12@naver.com>
2026-02-17 18:07:10 +0900

    chore: enable Sentry only in production environment

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,6 +5,7 @@
 import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
+  enabled: process.env.NODE_ENV === 'production',
   dsn: 'https://7fa572f95889655bbf4feb81c0e9b871@o4510871900651520.ingest.us.sentry.io/4510871914676224',
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
