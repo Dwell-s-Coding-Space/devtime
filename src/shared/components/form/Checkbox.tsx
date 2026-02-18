8bdb7190b0060907f55fca1c8ff1821c27103f2b
rachel490 <ccori12@naver.com>
2026-02-18 20:18:24 +0900

    refactor: add variant support and cva styling to Checkbox

--- src/shared/components/form/Checkbox.tsx
+++ src/shared/components/form/Checkbox.tsx
@@ -1,29 +1,49 @@
+import { cva } from 'class-variance-authority';
 import { ComponentProps } from 'react';
 
 import { cn } from '@/src/shared/utils/cn';
 
 import { CheckIcon } from '../../assets/svg';
 
+export type CheckboxVariant = 'todo' | 'regular';
+
+const CheckboxVariant = cva('h-6 w-6', {
+  variants: {
+    variant: {
+      regular: ['rounded-[5px] border border-primary bg-background-white'],
+      todo: ['rounded-[8px] border-[1.5px] border-white'],
+    },
+    checked: {
+      true: 'flex items-center justify-center',
+      false: '',
+    },
+  },
+  compoundVariants: [
+    {
+      variant: 'regular',
+      checked: true,
+      className: 'bg-background-primary-light text-text-primary',
+    },
+    {
+      variant: 'todo',
+      checked: true,
+      className: 'bg-background-white/50 text-text-white',
+    },
+  ],
+});
+
 interface CheckboxProps extends Omit<ComponentProps<'input'>, 'type'> {
-  text?: string;
+  variant?: CheckboxVariant;
 }
 
-const Checkbox = ({ checked, className, ...props }: CheckboxProps) => {
+const Checkbox = ({ variant = 'regular', checked, className, ...props }: CheckboxProps) => {
   return (
-    <label className="h-fit w-fit cursor-pointer">
-      <input type="checkbox" className="sr-only" checked={checked} {...props} />
-      <div
-        className={cn(
-          'border-border-white bg-background-primary h-7 w-7 rounded-[8px] border-[1.5px]',
-          {
-            'border-border-white bg-background-white/50 flex items-center justify-center': checked,
-          },
-          className
-        )}
-      >
-        {checked && <CheckIcon className={cn({ 'text-text-white': checked })} />}
+    <span className="h-fit w-fit cursor-pointer">
+      <input type="checkbox" className="sr-only" {...props} />
+      <div className={cn(CheckboxVariant({ variant, checked: checked }), className)}>
+        {checked && <CheckIcon />}
       </div>
-    </label>
+    </span>
   );
 };
 
