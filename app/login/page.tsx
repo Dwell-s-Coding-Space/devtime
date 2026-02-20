a0e0f27fff24c442a5c41a99ce2adde9cfd41eb8
rachel490 <ccori12@naver.com>
2026-02-21 02:35:39 +0900

    perf: remove priority prop from login page logo image

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -20,7 +20,7 @@ export default async function Login() {
         }}
       />
       <div className="flex h-[598px] w-[500px] flex-col items-center gap-12 rounded-[10px] bg-white/50 pt-18 shadow-[0_40px_100px_40px_rgba(3,104,255,0.05)] backdrop-blur-[25px]">
-        <Image src="/logo-vertical-primary.svg" alt="logo" width={132} height={100} priority />
+        <Image src="/logo-vertical-primary.svg" alt="logo" width={132} height={100} />
         <LoginForm />
       </div>
     </div>
