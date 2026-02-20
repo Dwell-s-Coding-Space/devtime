aa299268d402e8ceaa9777be299e29ea90987057
rachel490 <ccori12@naver.com>
2026-02-20 15:47:32 +0900

    refactor: separate server/client QueryClient and export getQueryClient

--- src/shared/providers/QueryProvider.tsx
+++ src/shared/providers/QueryProvider.tsx
@@ -1,30 +1,74 @@
 'use client';
 
-import { MutationCache, QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  defaultShouldDehydrateQuery,
+  isServer,
+  MutationCache,
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ReactNode } from 'react';
 
 import { UnauthorizedError } from '../api/error';
 import { ROUTES } from '../constants/routes';
 
-const queryClient = new QueryClient({
-  queryCache: new QueryCache({
-    onError: error => {
-      if (error instanceof UnauthorizedError) {
-        window.location.href = ROUTES.LOGIN;
-      }
+function makeClientQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
     },
-  }),
-  mutationCache: new MutationCache({
-    onError: error => {
-      if (error instanceof UnauthorizedError) {
-        window.location.href = ROUTES.LOGIN;
-      }
+    queryCache: new QueryCache({
+      onError: error => {
+        if (error instanceof UnauthorizedError) {
+          window.location.href = ROUTES.LOGIN;
+        }
+      },
+    }),
+    mutationCache: new MutationCache({
+      onError: error => {
+        if (error instanceof UnauthorizedError) {
+          window.location.href = ROUTES.LOGIN;
+        }
+      },
+    }),
+  });
+}
+
+function makeServerQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 5 * 60 * 1000,
+      },
+      dehydrate: {
+        shouldDehydrateQuery: query =>
+          defaultShouldDehydrateQuery(query) || query.state.status === 'pending',
+        shouldRedactErrors: error => {
+          return false;
+        },
+      },
     },
-  }),
-});
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+export function getQueryClient() {
+  if (isServer) {
+    return makeServerQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeClientQueryClient();
+    return browserQueryClient;
+  }
+}
 
 const QueryProvider = ({ children }: { children: ReactNode }) => {
+  const queryClient = getQueryClient();
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}
