bb487d7667767f716d72041c6ba58e513dc5a34c
rachel490 <ccori12@naver.com>
2026-02-20 19:49:13 +0900

    chore: re-export ProfileSettingForm from mypage public API

--- src/features/mypage/index.ts
+++ src/features/mypage/index.ts
@@ -1,2 +1,3 @@
 export { default as MyPageBoundary } from './components/MyPageBoundary';
 export { default as MyPageEditBoundary } from './components/MyPageEditBoundary';
+export { default as ProfileSettingForm } from './components/ProfileSettingForm';
